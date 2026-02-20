76196d4350f1d98708a5fe81cb46da13a74ca43b
oc-matti-rawlins-25 <matti.rawlins@student.onslow.school.nz>
2026-02-20 02:01:27 +0000

    Working on function challenge

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,6 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-
+let currentEggStock = 0
 
 func eggMenu() {
 
@@ -11,10 +11,24 @@ print("3. Show current stock")
 print("4. Show current eggs sold")
 print("5. Exit")
 print("Choose an option:")
+
 }
 
 func  addEggs(eggsInStock: Int, amount: Int) { 
-var eggsInStock = 0
+let lowerEggsLimit = 0
+let higherEggsLimit = 1000
+print("How many eggs would you like to add?")
+if let addEggs = readLine(), let numberOfEggs = Int(addEggs), numberOfEggs < higherEggsLimit, numberOfEggs > lowerEggsLimit {
+
+
+print("You have added \(numberOfEggs) to the total egg stock")
+let viewEggsStocks = numberOfEggs + currentEggStock
+} else {
+print("That is an invalid input")
+addEggs(eggsInStock: currentEggStock, amount: 0)
+
+
+}
 
 }
 
@@ -22,19 +36,21 @@ var eggsInStock = 0
 struct SwiftPlayground {
     static func main() {
 
-        var isRunning = true
-        let minimum = 1
-        let maximum = 5
+        let isRunning = true
+        let minimum = 0
+        let maximum = 6
 
         while isRunning == true{
         eggMenu()
-        if let userInput = readLine(), var Option = Int(userInput), Option > minimum, Option < maximum {
+        if let userInput = readLine(), let Option = Int(userInput), Option > minimum, Option < maximum {
         
         if Option == 1 {
-        print("How many eggs would you like to add?")
-        let userInput = readLine(), numberOfEggs = Int(userInput)
+        addEggs(eggsInStock: 0, amount: 0) 
         }
+        if Option == 3 {
+        print("current eggs stocks are \(currentEggStock)")
         }
+        } 
         else {
         print("Select a valid option")
         return(eggMenu())

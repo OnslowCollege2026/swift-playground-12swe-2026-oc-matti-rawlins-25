46d9c9fd8d1a116c6c21061590562610b2077044
oc-matti-rawlins-25 <matti.rawlins@student.onslow.school.nz>
2026-03-13 02:07:59 +0000

    battleship kind of working

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,52 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func printBoard() {
+    var isRunning = true
+        let size = 6
+var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
+var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+
+        print("Welcome to Battleships!")
+ocean.forEach { row in
+print(row)
+
+}
+print("---------------------------------------------")
+guesses[1][1] = "S"
+guesses[5][2] = "S"
+guesses.forEach { row in
+print (row)
+}
+while isRunning == true {
+print("select your row between 1 - 6")
+        let userInput = readLine()!
+        let rowNumber = Int(userInput)! - 1
+
+print("select your column between 1 - 6")
+        let userInput2 = readLine()!
+        let columnNumber = Int(userInput2)! - 1
+
+        ocean[rowNumber][columnNumber] = "S"
+ocean.forEach { row in
+print(row)
+
+} 
+else 
+
+print("---------------------------------------------")
+guesses[1][1] = "S"
+guesses[5][2] = "S"
+guesses.forEach { row in
+print (row)
+}
+} 
+}
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        printBoard()
+
+
     }
 }

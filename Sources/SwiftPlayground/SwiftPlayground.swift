53013332e352140d2697b8f5ef0c16478244bbe7
oc-matti-rawlins-25 <matti.rawlins@student.onslow.school.nz>
2026-03-20 01:21:58 +0000

    changes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,28 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func gameMenu() { 
+    print("Welcome to incredible text based adventure!!!!!")
+    print("Indie game fr fr")
+print("")
+    print("")
+    print("1.Start new game")
+    print("2.Wipe data")
+    print("3.See progress")
+    print("4.Game advice")
+    print("5.Quit")
+
+    guard let userInput = readLine() else { 
+if userInput >= 1, if userInput <= 5
+    } 
+    
+    
+    
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+    
     }
 }

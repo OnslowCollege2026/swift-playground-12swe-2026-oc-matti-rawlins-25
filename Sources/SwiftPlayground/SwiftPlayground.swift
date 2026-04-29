fab1df1dd9545dd5cbbd89fd4355addf205c221c
oc-matti-rawlins-25 <matti.rawlins@student.onslow.school.nz>
2026-04-29 22:45:26 +0000

    Fixed issue with the container being ableto hold over 50kgs of kumara

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -39,19 +39,15 @@ func addKumara(kumaraInStock: Double) {
     print("How many kg of kumara would you like to add?")
     if let addKumara = readLine(), let numberOfKumara = Double(addKumara),
         numberOfKumara >= higherKumaraLimit,
-        numberOfKumara > lowerKumaraLimit
+        numberOfKumara > lowerKumaraLimit, currentKumaraStock + numberOfKumara != aboveKumaraLimit
     { 
-        if numberOfKumara + currentKumaraStock == aboveKumaraLimit{
-            print("That is an invalid input")
-        addKumara(kumaraInStock: currentKumaraStock)
-        }
         //adds the user input into the current kumara stall stock
         print("You have added \(numberOfKumara)kg to the kumara container")
         currentKumaraStock = numberOfKumara + currentKumaraStock
         //if users input is invalid then makes the user reinput
     } else {
         print("That is an invalid input")
-        addKumara(kumaraInStock: currentKumaraStock)
+        
 
     }
     ///create the func that lets the user buy kumara

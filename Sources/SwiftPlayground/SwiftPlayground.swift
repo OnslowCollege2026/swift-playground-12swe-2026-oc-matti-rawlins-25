989c79202596c663237e5c185e4794b6f1039051
oc-matti-rawlins-25 <matti.rawlins@student.onslow.school.nz>
2026-04-29 23:13:33 +0000

    commiting minor change

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -147,7 +147,7 @@ func addKumara(kumaraInStock: Double) {
 
                     if Option == 4 {
                         print("Previous sales")
-// forEach.ownerInfoBags
+// forEach.ownerInfoBags slide 7 last year
                         print("The customer brought\(ownerInfoBags) bags and \(ownerInfoKumara)kgs of kumara")
 
                         print("The average weight sold is ")

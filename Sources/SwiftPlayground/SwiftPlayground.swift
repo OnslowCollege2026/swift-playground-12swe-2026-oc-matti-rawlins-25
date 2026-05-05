6e420e013832d87a84fbed446440695d1101fc0c
oc-matti-rawlins-25 <matti.rawlins@student.onslow.school.nz>
2026-05-05 01:09:23 +0000

    gogo

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -21,12 +21,12 @@ nonisolated(unsafe)
 //create variable to store the amount of bags brought
 //nonisolated means the program won't crash because this variable is in a shared mutated state
 nonisolated(unsafe)
-    var ownerInfoBags = [0.01]
+    var ownerInfoBags = [0.0]
 
 //create variable to store the amount of kumara brought
 //nonisolated means the program won't crash because this variable is in a shared mutated state
 nonisolated(unsafe)
-    var ownerInfoKumara = [0.01]
+    var ownerInfoKumara = [[0.0]]
 
 ///creates a func to allow the user to add kumara to the shop
 func addKumara(kumaraInStock: Double) {
@@ -37,7 +37,7 @@ func addKumara(kumaraInStock: Double) {
     //sets a higher limit for amount of kumara that can be inside the kumara stall
     let higherKumaraLimit = 50.0
 
-//setting the invalid upper boundary
+    //setting the invalid upper boundary
     let aboveKumaraLimit = 50.1
 
     //Ask the user for their input and sets requirements for their input
@@ -45,14 +45,13 @@ func addKumara(kumaraInStock: Double) {
     if let addKumara = readLine(), let numberOfKumara = Double(addKumara),
         numberOfKumara <= higherKumaraLimit,
         numberOfKumara > lowerKumaraLimit, currentKumaraStock + numberOfKumara != aboveKumaraLimit
-    { 
+    {
         //adds the user input into the current kumara stall stock
         print("You have added \(numberOfKumara)kg to the kumara container")
         currentKumaraStock = numberOfKumara + currentKumaraStock
         //if users input is invalid then makes the user reinput
     } else {
         print("That is an invalid input")
-        
 
     }
     ///create the func that lets the user buy kumara
@@ -76,30 +75,33 @@ func addKumara(kumaraInStock: Double) {
         //asks user for their input and puts requirements
         print("How many kumara do you want to buy in kgs ($3 per kg)?")
         if let buyKumara = readLine(), let buyKumaraNumber = Double(buyKumara),
-            buyKumaraNumber <= buyKumaraHigherLimit, buyKumaraNumber >= buyKumaraLowerLimit{
-        
+            buyKumaraNumber <= buyKumaraHigherLimit, buyKumaraNumber >= buyKumaraLowerLimit
+        {
+
             // turns amount of kumara brought into money
-            
+
             let costOfKumara = Double(buyKumaraNumber) * 3.0
 
             //converting Int to double to adjust stock
             let kumaraNumber = Double(buyKumaraNumber)
 
-        print("how many bags would you like to buy (Bags cost 0.20 and hold 5kgs) ?")
-        if let buyBags = readLine(), let buyBagsNumber = Int(buyBags), buyBagsNumber <= buyBagsUpperLimit, buyBagsNumber >= buyBagsLowerLimit{
-            
-            let bagsBroughtQuanity = Double(buyBagsNumber)
-            let costOfBags = bagsBroughtQuanity * 0.20
-            let totalCost = costOfBags + costOfKumara
-            print("The cost for \(buyKumaraNumber)kgs of kumara brought is $\(costOfKumara)")
-            print("The cost for \(bagsBroughtQuanity)bags is $\(costOfBags)")
-            print("The total cost is $\(totalCost)")
-        ownerInfoBags.append(bagsBroughtQuanity)
-        ownerInfoKumara.append(kumaraNumber)
-            }else {
-            //if users input is invalid informs user and makes them go again
-            print("That is an invalid input")
-            return sellKumara()
+            print("how many bags would you like to buy (Bags cost 0.20 and hold 5kgs) ?")
+            if let buyBags = readLine(), let buyBagsNumber = Int(buyBags),
+                buyBagsNumber <= buyBagsUpperLimit, buyBagsNumber >= buyBagsLowerLimit
+            {
+
+                let bagsBroughtQuanity = Double(buyBagsNumber)
+                let costOfBags = bagsBroughtQuanity * 0.20
+                let totalCost = costOfBags + costOfKumara
+                print("The cost for \(buyKumaraNumber)kgs of kumara brought is $\(costOfKumara)")
+                print("The cost for \(bagsBroughtQuanity)bags is $\(costOfBags)")
+                print("The total cost is $\(totalCost)")
+                ownerInfoBags.append(bagsBroughtQuanity)
+                ownerInfoKumara.append([kumaraNumber])
+            } else {
+                //if users input is invalid informs user and makes them go again
+                print("That is an invalid input")
+                return sellKumara()
             }
             currentKumaraStock = currentKumaraStock - kumaraNumber
         } else {
@@ -107,6 +109,23 @@ func addKumara(kumaraInStock: Double) {
             print("That is an invalid input")
             return sellKumara()
         }
+    }
+    func ownersSummary() {
+        func total(of matrix: [[Double]]) -> Double {
+            var sum = 0.0
+            for row in ownerInfoKumara {
+                for value in row {
+                    sum += value
+                }
+            }
+            return sum
+        }
+
+        let ownerInfoKumara = [
+            [0.0],[0.0],[0.0],[0.0],[0.0],[0.0]
+            ]
+
+        print(total(of: ownerInfoKumara))
     }
 
     @main
@@ -151,47 +170,27 @@ func addKumara(kumaraInStock: Double) {
                     }
 
                     if Option == 4 {
-                        print("Previous sales")
-                        ownerInfoBags.enumerated().forEach { index, number in
-                        let doubleNumber = Double(number)
-                        if doubleNumber == 0.01{
-                            ownerInfoBags.remove(at: index)
+                        ownersSummary()
+
+                        if Option == 5 {
+
+                            //thanks the user for buying from the kumara stall
+                            print("Thank you for purchasing from the kumara stall")
+
+                            //stops the while loop when the user exits the interfact
+                            isRunning = false
+
                         }
-print("The \(index+1) customer brought\(number)bags")
-                        }
-                        ownerInfoKumara.enumerated().forEach { index, number in
-                        let doubleNumber2 = Double(number)
-                        if doubleNumber2 == 0.01 {
-                            ownerInfoKumara.remove(at: index)
-                        }
-print("The \(index+1) customer brought \(number)kgs of kumara")
-                        }
-let totalSalesWeight = ownerInfoKumara
-let totalBagNumber = ownerInfoBags
-print(totalSalesWeight)
-print(totalBagNumber)
-//check how to add all part of an array together
-                        print("The average weight sold is ")
-                        print("The average amount made per bag is ")
+                    } else {
+
+                        //if users input is invalid tell them and makes them run the code again
+                        print("Select a valid option")
+                        continue
                     }
 
-                    if Option == 5 {
-
-                        //thanks the user for buying from the kumara stall
-                        print("Thank you for purchasing from the kumara stall")
-
-                        //stops the while loop when the user exits the interfact
-                        isRunning = false
-
-                    }
-                } else {
-
-                    //if users input is invalid tell them and makes them run the code again
-                    print("Select a valid option")
-                    continue
-                    }
-                        
-                    }
                 }
             }
         }
+
+    }
+}
